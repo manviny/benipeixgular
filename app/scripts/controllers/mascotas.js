36c52d44c5d53b39df5b382f6b72e1b868ad2f7c
manviny <manol@manol.home>
2014-10-30 18:36:23 +0100

    installed ui-grid and bootstrap

--- app/scripts/controllers/mascotas.js
+++ app/scripts/controllers/mascotas.js
@@ -12,5 +12,36 @@ angular.module('benipeixgularApp')
   		pwPage.getPage('1199')
   		.then(function(response){
   			console.debug("Mis datos", response);
+  			$scope.myData = [];
+  			$scope.myData.push(response)
   		})
+  		pwPage.getChildren('1011', 'template=mascota', '')		//mascotas
+  		.then(function(response){
+  			console.debug("Mis datos", response);
+  			// $scope.myData = [];
+  			// $scope.myData.push(response)
+  		})
+
+
+		//   $scope.myData = [
+		//     {
+		//         "firstName": "Cox",
+		//         "lastName": "Carney",
+		//         "company": "Enormo",
+		//         "employed": true
+		//     },
+		//     {
+		//         "firstName": "Lorraine",
+		//         "lastName": "Wise",
+		//         "company": "Comveyer",
+		//         "employed": false
+		//     },
+		//     {
+		//         "firstName": "Nancy",
+		//         "lastName": "Waters",
+		//         "company": "Fuelton",
+		//         "employed": false
+		//     }
+		// ];
+
   });
