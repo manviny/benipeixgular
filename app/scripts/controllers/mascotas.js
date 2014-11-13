866cae399988a78a7f2bb8a01ff25fde9188526f
jeison stiven molina tirado <jeisonxvlc@gmail.com>
2014-11-13 18:30:39 +0100

    Config Get-page
    Se ha configurado get-page para que te devuelva el nombre de las
    PageArray que te devuelva process, agregado y comienzo de configuración
    de la tabla con los buscadores junto el calendario

--- app/scripts/controllers/mascotas.js
+++ app/scripts/controllers/mascotas.js
@@ -27,15 +27,6 @@ angular.module('benipeixgularApp')
   			// $scope.myData = [];
   			// $scope.myData.push(response)
   		})
-
-  		pwPage.createIds('template=desplegable')		//mascotas
-  		.then(function(response){
-  			$scope.myIds = [];
-			console.log("Mis datosa", response);
-			$scope.myIds.push(response)
-  		})
-
-
 		//   $scope.myData = [
 		//     {
 		//         "firstName": "Cox",
@@ -57,12 +48,49 @@ angular.module('benipeixgularApp')
 		//     }
 		// ];
 
+	  $scope.today = function() {
+	  $scope.dt = new Date();
+	  };
+	  $scope.today();
+
+	  $scope.clear = function () {
+	    $scope.dt = null;
+	  };
+
+	  // Disable weekend selection
+	  $scope.disabled = function(date, mode) {
+	    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+	  };
+
+	  $scope.toggleMin = function() {
+	    $scope.minDate = $scope.minDate ? null : new Date();
+	  };
+	  $scope.toggleMin();
+
+	  $scope.open = function($event) {
+	    $event.preventDefault();
+	    $event.stopPropagation();
+
+	    $scope.opened = true;
+	  };
+
+	  $scope.dateOptions = {
+	    formatYear: 'yy',
+	    startingDay: 1
+	  };
+
+	  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+	  $scope.format = $scope.formats[0];
+
+
 
     $scope.rowCollection = [
         {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
         {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
         {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
     ];
+
+
 
 
   });
